0fc7ea09d8fbe991cc108d4b2fda5a6657af03e4
iequa <artyom71xd@gmail.com>
2023-07-11 23:11:11 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//Глобальные поля приложения
+
+//Игровая матрица значений
+let fieldsValues = [0,0,0][0,0,0][0,0,0];
+//
+let currentPlayer = false;
+let enemyVariant = "Player";
+
+const App = ()=> {
+    const onButtonClickEvent = onButtonClickEvent();
+    const onClickEvent = onClickEvent();
+};
+
+function onButtonClickEvent() {
+    //customElements.define("TicTacDiv", TicTacDiv)
+    console.log("this is test function! a = " + a);
+    a++;
+};
+
+function onClickEvent() {
+    //customElements.define("TicTacDiv", TicTacDiv)
+    console.log("Div click! " + a);
+    a++;
+};
+//export {App};
