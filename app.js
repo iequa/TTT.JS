da60e261a8d2f218bd18c7a22bcbe4816dab47bf
iequa <artyom71xd@gmail.com>
2023-07-12 00:45:43 +0300

    Макет поля игры, базовая логика считывания клика

--- app.js
+++ app.js
@@ -2,24 +2,26 @@
 
 //Игровая матрица значений
 let fieldsValues = [0,0,0][0,0,0][0,0,0];
-//
+//Сторона, которая делает ход в текущий момент
 let currentPlayer = false;
+//Тип противника (компьютер, человек)
 let enemyVariant = "Player";
 
-const App = ()=> {
-    const onButtonClickEvent = onButtonClickEvent();
-    const onClickEvent = onClickEvent();
-};
+const buttonHandler = start_button.onclick = onClickHandler;
+const divHandler = allDivFields.onclick = onClickHandler;
+
+function onClickHandler(event) {
+    console.log("это кликнул " + event.srcElement.id);
+    currentPlayer = true;
+}
 
 function onButtonClickEvent() {
     //customElements.define("TicTacDiv", TicTacDiv)
-    console.log("this is test function! a = " + a);
-    a++;
+    console.log("this is test function! cp = " + currentPlayer);
 };
 
 function onClickEvent() {
     //customElements.define("TicTacDiv", TicTacDiv)
-    console.log("Div click! " + a);
-    a++;
+    console.log("Div click! " + currentPlayer);
 };
 //export {App};
