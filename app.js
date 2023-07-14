0cbaae49827f34b409f2cd870a5998b685919079
iequa <artyom71xd@gmail.com>
2023-07-15 00:14:27 +0300

    Режимы игры, простые ходы компьютера, рефакторинг

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const BOT_LINE_HORIZONTAL = [6, 7, 8];
 const LEFT_LINE_VERTICAL = [0, 3, 6];
 const MIDDLE_LINE_VERTICAL = [1, 4, 7];
 const RIGHT_LINE_VERTICAL = [2, 5, 8];
+////////////////
 
 //Игровая матрица значений
 let fieldsValues = 
@@ -17,44 +18,76 @@ let fieldsValues =
     0,0,0,
     0,0,0
 ];
+////////////////
+//Количество свободных для записи полей.
+let availableFields = 9;
+////////////////
+
 //Сторона, которая делает ход в текущий момент (true - X, false - o)
-let currentPlayer = true;
+let mainPlayerTurn = true;
+////////////////
+
 let someoneWin = false;
 
+//Счёт
 let scoreX = 0;
 let scoreO = 0;
-//Тип противника (компьютер, человек)
-let enemyVariant = "Player";
+////////////////
+
+//Тип противника (человек - true, компьютер - false)
+let pvpMode = true;
+////////////////
+
 //Шаблон для отрисовки крестика
 const X_ELEMENT = "<div class=\"x\"></div>";
+////////////////
+
 //Шаблон для отрисовки нолика
 const O_ELEMENT = "<div class=\"o\"></div>";
+////////////////
 
-const buttonHandler = start_button.onclick = buttonClickHandler;
-const divHandler = allDivFields.onclick = divClickHandler;
+//Задаём обработчики событий для элементов
+start_button.onclick = buttonClickHandler;
+allDivFields.onclick = divClickHandler;
+pvp.onclick = gamemodeClickHandler;
+pve.onclick = gamemodeClickHandler;
+////////////////
 
+//Основной обработчик. Обрабатывает клик по игровому полю.
 function divClickHandler(event) {
-    //Проверяем класс кликнутого объекта, если это <div> то производим отрисовку
-    if ( !someoneWin && event.srcElement.className !== "x" && event.srcElement.className !== "o") {
-        let currentDiv = document.getElementById(event.srcElement.id);
-        currentDiv.innerHTML = currentPlayer ? X_ELEMENT : O_ELEMENT;
+    
+    //Проверяем класс кликнутого объекта, если это <divs_top*> то обрабатываем событие.
+    if (!someoneWin && event.srcElement.className !== "x" && event.srcElement.className !== "o") {
+        //Выбираем позицию установки
+        const id = selectOrComputePlaceId(pvpMode, mainPlayerTurn) ? event.srcElement.id : computeIdAndPlace(fieldsValues);
+        //Получаем выбранный див
+        let currentDiv = document.getElementById(id);
+        //Отрисовываем объект
+        currentDiv.innerHTML = mainPlayerTurn ? X_ELEMENT : O_ELEMENT;
+        //Берём последний символ из id выбранного объекта, выставляем значение в массив
         const fieldIndex = parseInt(currentDiv.id.charAt(currentDiv.id.length-1));
-        fieldsValues[fieldIndex-1] = currentPlayer ? 1 : 2;
-        someoneWin = checkFieldsValues(fieldsValues, currentPlayer);
-        if (someoneWin) {
-            //Нагло лезем в главный див отрисовать победное окно
-            let baseDiv = document.getElementById("board");
-            const winner = currentPlayer ? "Крестик" : "Нолик";
-            currentPlayer ? scoreX++ : scoreO++;
-            baseDiv.innerHTML = `\"Крестики\"|${scoreX}:${scoreO}|\"Нолики\"` + `<p class="win_text">${winner} победил! </p>`;
-        }
-        currentPlayer = !currentPlayer;
+        fieldsValues[fieldIndex-1] = mainPlayerTurn ? 1 : 2;
+
+        //Проверяем поле на победу одной из сторон
+        someoneWin = checkFieldsForWinner(fieldsValues, mainPlayerTurn);
+
+        //Ход другого игрока
+        mainPlayerTurn = !mainPlayerTurn;
+
+        //Уменьшаем количество доступных полей
+        availableFields--;
+    }
+    //Автоход компьютера
+    if (!someoneWin && !pvpMode && !mainPlayerTurn && availableFields>0) {
+        divClickHandler(event);
     }
 }
 
+//Обработчик кнопки сброса состояния игры
 function buttonClickHandler() {
+    availableFields = 9;
     fieldsValues = [0,0,0,0,0,0,0,0,0];
-    currentPlayer = true;
+    mainPlayerTurn = true;
     for(let i = 1; i<=fieldsValues.length;i++) {
         const elem = document.getElementById("divs_top"+i);
         elem.innerHTML = "";
@@ -63,7 +96,34 @@ function buttonClickHandler() {
     someoneWin = false;
 };
 
-function checkFieldsValues(fieldsValues, lastPlayer) {
+//Обработчик режима игры
+function gamemodeClickHandler(event) {
+    pvpMode = event.currentTarget.value === "true";
+}
+
+//Вспомогательная функция выбора ID для див'а
+function selectOrComputePlaceId(pvp, mainPlayerTurn) {
+    if (!pvp) {
+        if (!mainPlayerTurn) {
+            return false;
+        } else {
+            return true;
+        }    
+    }
+    return true;
+}
+
+//Рандомные ходы для компьютера
+function computeIdAndPlace(fieldsValues) {
+    let place = 4;
+    if (fieldsValues[place] !== 0) {
+        place = getRandomInt(8);
+    }
+    return `divs_top${place+1}`;
+}
+
+//Проверка игрового поля на предмет выигрыша одной из сторон
+function checkFieldsForWinner(fieldsValues, lastPlayer) {
     const lastPlayerIndex = lastPlayer ? 1 : 2;
     if(
         arrayCheck(fieldsValues, FROM_LEFT_TOP_CORNER_TO_RIGHT_BOT, lastPlayerIndex) ||
@@ -74,12 +134,18 @@ function checkFieldsValues(fieldsValues, lastPlayer) {
         arrayCheck(fieldsValues, LEFT_LINE_VERTICAL, lastPlayerIndex) ||
         arrayCheck(fieldsValues, MIDDLE_LINE_VERTICAL, lastPlayerIndex) ||
         arrayCheck(fieldsValues, RIGHT_LINE_VERTICAL, lastPlayerIndex) 
-        ) {
+    )   {
+            //Отрисовываем победное окно в окне с результатами
+            let baseDiv = document.getElementById("board");
+            const winner = mainPlayerTurn ? "Крестик" : "Нолик";
+            mainPlayerTurn ? scoreX++ : scoreO++;
+            baseDiv.innerHTML = `\"Крестики\"|${scoreX}:${scoreO}|\"Нолики\"` + `<p class="win_text">${winner} победил! </p>`;
             return true;
     }
     return false;
 }
 
+//Вспомогательный метод для проверки выигрышного расположения одной из сторон
 function arrayCheck(fieldsValues, indexes, lastPlayerIndex) {
     if( fieldsValues[indexes[0]] === lastPlayerIndex && 
         fieldsValues[indexes[1]] === lastPlayerIndex && 
@@ -89,3 +155,7 @@ function arrayCheck(fieldsValues, indexes, lastPlayerIndex) {
     }
     return false;
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
